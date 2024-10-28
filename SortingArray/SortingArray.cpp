0e95b67d06d5cf30ac44dc3a4de700a6dcd5634f
BelalBasal <basdibrahim@gmail.com>
2024-10-28 16:19:46 +0300

    Test

--- SortingArray/SortingArray.cpp
+++ SortingArray/SortingArray.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm> 
 using namespace std;
 void Sorting(int arr[], int size)
 {
@@ -15,8 +16,31 @@ void Sorting(int arr[], int size)
         }
     }
 }
-int main()
+bool Test()
 {
    
+    int arr[10];
+    int cpy[10];
+    bool Flag = true;
+    for (int i = 0; i < 1000;i++)
+    {
+        for (int j = 0; j < 10;j++)
+        {
+            cpy[j] = arr[j] = rand() % 100;
+
+        }
+        Sorting(arr, 10);
+        sort(cpy, cpy + 10);
+        for (int i = 0; i < 10; i++)
+        {
+            Flag &= (arr[i] == cpy[i]);
+        }
+
+    }return Flag;
+}
+int main()
+{
+    bool test = Test();
+    cout << test;
 }
 
